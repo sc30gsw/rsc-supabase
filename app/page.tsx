58707f08530da650a464b36665ce3a6d17d8b729
sc30gsw <haidoushi875@gmail.com>
2023-08-11 09:48:37 +0900

    SSR実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import NotesList from './components/notes-list'
+
 const Page = () => {
   return (
     <main>
-      <div className="m-10 text-center">Hello World🚀</div>
+      <div className="m-10 text-center">
+        <p>Hello World🚀</p>
+        <NotesList />
+      </div>
     </main>
   )
 }
